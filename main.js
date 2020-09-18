802fe5cb07a57be638296e363690dff3655d6713
Paul <paul@eso.com.tw>
2020-09-18 14:05:06 +0800

    0918修改

--- main.js
+++ main.js
@@ -4,10 +4,6 @@ $(document).ready(function() {
   document.body.prepend(close)
 })
 
-$('.search_btn').click(function () {
-  $('.search_bar').toggleClass('active');
-});
-
 $(window).scroll(function() {
     let win_h = $(document).scrollTop();
     let header_h = $('.slider_block').offset().top;
@@ -32,11 +28,32 @@ $('.go_to').click(function () {
 $('.mobile_nav').click(function () {
   $('.mobile_nav').toggleClass('active');
   $('.mobile_menu').toggleClass('active')
-  $('.mobile_menu_close').toggleClass('active')
+
+  if($('.search_bar').hasClass('active') || $('.mobile_menu').hasClass('active')){
+    $('.mobile_menu_close').addClass('active')
+  }else{
+    $('.mobile_menu_close').removeClass('active')
+  }
+});
+
+$('.search_btn').click(function () {
+  $('.search_bar').toggleClass('active');
+
+  if($('.search_bar').hasClass('active') || $('.mobile_menu').hasClass('active')){
+    $('.mobile_menu_close').addClass('active')
+  }else{
+    $('.mobile_menu_close').removeClass('active')
+  }
+  
 });
 
 $(document).on('click','.mobile_menu_close',function(){
-  $('.mobile_nav').toggleClass('active');
-  $('.mobile_menu').toggleClass('active')
-  $('.mobile_menu_close').toggleClass('active')
+  $('.mobile_nav').removeClass('active');
+  $('.mobile_menu').removeClass('active')
+  $('.mobile_menu_close').removeClass('active')
+  $('.search_bar').removeClass('active');
+})
+
+$('li.m_sub').click(function () {
+  $(this).toggleClass('active')
 })
