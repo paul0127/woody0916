950ac39bf1fef1172fa11fc9d28b3e5037a696f8
Paul <paul@eso.com.tw>
2020-09-28 17:43:51 +0800

    0928修改

--- Scripts/main.js
+++ Scripts/main.js
@@ -227,14 +227,8 @@ $(document).on('click', '.search_btn', function () {
 })
 
 /*點擊遮幕關閉選單及搜尋*/
-$(document).on('click', '.mobile_menu_close', function () {
-  $('.mobile_nav').removeClass('active')
-  $('.mobile_menu').removeClass('active')
-  $('.mobile_menu_close').removeClass('active')
-  $('.search_bar').removeClass('active')
-})
-/*手機touch遮幕關閉選單及搜尋*/
-$(document).on('tap', '.mobile_menu_close', function () {
+$(document).on('vclick', '.mobile_menu_close', function () {
+  console.log(0)
   $('.mobile_nav').removeClass('active')
   $('.mobile_menu').removeClass('active')
   $('.mobile_menu_close').removeClass('active')
